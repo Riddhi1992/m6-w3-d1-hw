1cb6c30800864ad30ac50646ed95e7701f7b0674
Riddhi <riddhipatel.csu@gmail.com>
2021-06-20 13:24:59 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled from 'styled-components';
+import theme from "styled-theming";
+import { Provider as ReduxProvider } from "react-redux";
+import DarkThemeProvider from './DarkThemeProvider';
+import DarkThemeToggle from './DarkThemeToggle';
+import store from "./redux/store";
 
-function App() {
+//THEME DEFINITION
+export const theme1 = theme("theme", {
+  light: '#fff',
+  dark: '#2d2d2d',
+});
+
+export const theme2 = theme("theme", {
+  light: "#000",
+  dark: "#fff",
+
+});
+
+const Container = styled.div`
+background-color: ${theme1};
+display: flex;
+flex-direction: column;
+width: 100vw;
+height: 100vh;
+align-items:center;
+justify-content: center;
+font-family: sans-serif;
+`
+
+const SwitchTheme = styled.div`
+display: flex;
+height: 75px;
+align-items:center;
+padding: 0 50px;
+justify-content: space-between;
+`
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <React.Fragment>
+      <ReduxProvider store={store}>
+        <DarkThemeProvider theme = {{ theme:"light"}} >
+          <SwitchTheme>
+            <h1> Theme App </h1>
+            <p>
+              <DarkThemeToggle/>
+            </p>
+          </SwitchTheme>
+          <Container>
+            <h2>Welcome!</h2>
+            <h3> Full stack web dev</h3>
+          </Container>
+        </DarkThemeProvider>
+      </ReduxProvider> 
+    </React.Fragment>
+  )
+};
 
 export default App;
